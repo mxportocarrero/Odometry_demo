3c4ef2cb329b2719b698b0acab319c2c61eae5fd
Max Portocarrero <mxportocarrero@gmail.com>
2018-08-15 11:17:26 -0500

    programa de test terminado

--- t.cpp
+++ t.cpp
@@ -58,10 +58,56 @@ int main(){
 	// Dark blue background
 	glClearColor(0.0f, 0.0f, 0.4f, 0.0f); // Color de fondo en su forma normalizada
 
+	//
+	// Registering the VAOs
+	//
+	GLuint VertexArrayID;
+	glGenVertexArrays(1, &VertexArrayID); // El primer argumento debe ser el numero de VAOs
+	glBindVertexArray(VertexArrayID);
+
+	// Creamos y compilamos nuestro programa GLSL a partir de los shaders
+	GLuint programID = LoadShaders("SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader");
+
+	static const GLfloat g_vertex_buffer_data[] = {
+     -1.0f, -1.0f, 0.0f,
+      1.0f, -1.0f, 0.0f,
+      0.0f,  1.0f, 0.0f,
+	};
+
+	GLuint vertexbuffer;
+	glGenBuffers(1, &vertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+	//
+	// Fin de la Etapa de Registro
+	//
+
 	do{
       // clear the screen
       glClear( GL_COLOR_BUFFER_BIT );
 
+      // Use our shader
+      glUseProgram(programID);
+
+      /**Drawing stage**/
+      // 1st attribute buffer : vertices
+      glEnableVertexAttribArray(0);
+      glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+      glVertexAttribPointer(
+         0,          // attribute 0. No particular reason for 0, but must match the layout in the shader
+         3,          // size
+         GL_FLOAT,   // type
+         GL_FALSE,   // normalized?
+         0,          // stride
+         (void*)0    // array buffer offset
+      );
+
+      glDrawArrays(GL_TRIANGLES, 0, 3); // 3 indices starting at 0 -> 1 triangle
+
+      glDisableVertexAttribArray(0);
+
+      /**Final Stage: Refreshing buffers**/
       glfwSwapBuffers(window);
       glfwPollEvents(); // Revisa si se ha emitido algun evento
 
@@ -69,6 +115,11 @@ int main(){
 	// Check if the ESC key was pressed or the window was closed
 	while(glfwGetKey(window,GLFW_KEY_ESCAPE) != GLFW_PRESS &&
          glfwWindowShouldClose(window) == 0);
+
+   // Cleanup VBO
+   glDeleteBuffers(1, &vertexbuffer);
+   glDeleteVertexArrays(1, &VertexArrayID);
+   glDeleteProgram(programID);
 
 	glfwTerminate();
 
